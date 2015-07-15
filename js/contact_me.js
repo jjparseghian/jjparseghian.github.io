ebd81791501d7f2cda6bf0c2063fa8091df974fb
James Parseghian <jjparseghian@gmail.com>
2015-07-15 17:45:03 -0400

    Uncomment logic in ajax success and error

--- js/contact_me.js
+++ js/contact_me.js
@@ -34,27 +34,27 @@ $(function() {
                 // cache: false,
                 success: function(response) {
                     // Success message
-                    // $('#success').html("<div class='alert alert-success'>");
-                    // $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                    //     .append("</button>");
-                    // $('#success > .alert-success')
-                    //     .append("<strong>Your message has been sent. </strong>");
-                    // $('#success > .alert-success')
-                    //     .append('</div>');
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
 
-                    // //clear all fields
-                    // $('#contactForm').trigger("reset");
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
                     console.log("success", response);
                 },
                 error: function(error) {
                     // Fail message
-                    // $('#success').html("<div class='alert alert-danger'>");
-                    // $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                    //     .append("</button>");
-                    // $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    // $('#success > .alert-danger').append('</div>');
-                    // //clear all fields
-                    // $('#contactForm').trigger("reset");
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
                     console.log("error", error);
                 },
             })
