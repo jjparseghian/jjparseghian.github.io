a2ba37654c2c87f2f1047c16811431150bc84236
James Parseghian <jjparseghian@gmail.com>
2015-07-16 02:42:02 -0400

    Add captcha response string to data in ajax post to mail server and add additional status messages

--- js/contact_me.js
+++ js/contact_me.js
@@ -24,21 +24,51 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    captchaResp: window.grecaptcha.getResponse()
                 },
                 cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                success: function(response) {
+                    switch (response.message) {
+                        case 'success':
+                            // Success message
+                            $('#success').html("<div class='alert alert-success'>");
+                            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success > .alert-success')
+                                .append("<strong>Your message has been sent. </strong>");
+                            $('#success > .alert-success')
+                                .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                            //clear all fields
+                            $('#contactForm').trigger("reset");
+                            console.log("success", response);
+                            break;
+                        case 'failure_email':
+                            // Fail message
+                            $('#success').html("<div class='alert alert-danger'>");
+                            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                            $('#success > .alert-danger').append('</div>');
+                            //clear all fields
+                            $('#contactForm').trigger("reset");
+                            console.log("success", response);
+                            break;
+                        case 'failure_captcha':
+                            // reCaptcha Fail message
+                            $('#success').html("<div class='alert alert-danger'>");
+                            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                .append("</button>");
+                            $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that you are a robot. I don't want emails from robots. Please try again later!");
+                            $('#success > .alert-danger').append('</div>');
+                            //clear all fields
+                            $('#contactForm').trigger("reset");
+                            console.log("success", response);
+                            break;
+                        default:
+                            console.log("Something else is going horribly wrong!");
+                    }
                 },
                 error: function() {
                     // Fail message
